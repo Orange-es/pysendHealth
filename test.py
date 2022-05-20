879b91c0828d28be5d4785793cd7f1b775250628
Orange-es <54025960+Orange-es@users.noreply.github.com>
2022-05-20 19:53:48 +0800

    Update test.py
    之前弄好的，可惜不用了，用钉钉打卡

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/22 09:01
+# @Author  : JingBW
+# @Site    : 
+# @File    : test06_testAccount.py
+# @Software: PyCharm
 import json
+import smtplib
 import time
+from email.header import Header
+from email.mime.text import MIMEText
 
 import requests
+
+#可以正常打卡
 
 loginurl ='http://hmgr.sec.lit.edu.cn/wms/healthyLogin'
 uploadurl = 'http://hmgr.sec.lit.edu.cn/wms/addHealthyRecord'
@@ -11,8 +22,28 @@ headers ={
 "Content-Type": "application/json"}
 
 data = {
-        "cardNo": "B21043925",
-        "password": "b6c3c76ec8b95e67716188ca4488604d2d301b4804ded435dfedb08399e2f1ab"}
+        "cardNo": "",
+        "password": ""}
+
+# 用户列表，在这里补充用户名，密码，email.SHA256加密后的密码
+#下面都是账号都是错误 演示示例而已
+userlist =[{
+    "username": "B2192",
+    "password": "b6c3c4d2d301b4804ded435dfedb08399e2f1ab",
+    "email"   : "7861@qq.com"
+}
+    # ,
+    # {
+    # "username": "B24533",
+    # "password": "bf2365cd2bf0e58ffb26ec5f59b0f174e68b34d3ab8fd9e64",
+    # "email"   : "5138@qq.com"
+    # },
+    # {
+    #     "username": "B2926",
+    #     "password": "e7752534505f5cc9080d5ea0bc71ff55a8467",
+    #     "email": "2591@qq.com"
+    # }
+]
 
 # 登录
 def gettoken(data):
@@ -22,12 +53,12 @@ def gettoken(data):
         print("登录")
         print("请求成功:打印返回内容及token")
         s=json.loads(res.text)
-        print(s)
+        # print(s)
         print(s["data"]["token"])
     else:
         print('请求失败')
-    with open('./登录.json', 'w') as fp:
-        json.dump(res.text, fp)
+    # with open('./登录.json', 'w') as fp:
+    #     json.dump(res.text, fp)
     return s
 
 
@@ -88,16 +119,67 @@ def upload():
     updata["userId"]=s["data"]["userId"]
     updata["teamId"] = s["data"]["teamId"]
     print("打印修改后的header和updata")
-    print(headers)
+    # print(headers)
     print(time.strftime("%Y-%m-%d", time.localtime()))
     updata["reportDate"] = time.strftime("%Y-%m-%d", time.localtime())
     res = requests.post(url=uploadurl,headers=headers,data=json.dumps(updata))
     print(json.dumps(updata))
+    re=[]
     if res.status_code==200:
         print("提交成功")
-        print(res.text)
+        # print(res.text)
+        re.append('提交成功')
+        re.append(res.text)
+
     else:
         print('提交失败')
+        re.append('提交失败')
+    return re
 
 
-upload()
+
+def mail(receivers,content):
+    # 第三方 SMTP 服务
+    mail_host = "smtp.qq.com"  # 设置服务器
+    mail_user = "jingbw"  # 用户名
+    mail_pass = "xxxxxxi"  # 口令
+    sender = 'xxxxxx@qq.com' #发送邮件账号
+    receivers = receivers # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    # message = MIMEText(str(s['data']['list'][15]), 'plain', 'utf-8')
+    message = MIMEText(str(content[2])+','+str(content[0]), 'plain', 'utf-8')
+    message['From'] = Header("Jing", 'utf-8')
+    message['To'] = Header(str(content[2]), 'utf-8')
+
+    subject = "健康汇报,"+content[0]
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("邮件发送成功")
+    except Exception:
+        print("Error: 无法发送邮件")
+
+
+
+def main(event, context):
+    for i in range(len(userlist)):
+        data['cardNo'] = userlist[i]['username']
+        data['password'] = userlist[i]['password']
+        re=[userlist[i]['username'],'账号错误','账号错误']
+        try:
+            re = upload()
+        except Exception:
+            print(userlist[i]['username'],'账号运行异常')
+        stuId=userlist[i]['username']
+        re.append(stuId)
+        mail(userlist[i]['email'], re)
+        print(stuId,",打卡完成。")
+        print("Received context: " + str(context))
+
+
+if __name__ == '__main__':
+    main(event='',context='')
